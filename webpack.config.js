88625bc416a45283c81ed90df3aebc69e6c966ce
Malkov Nikifor <nikiformalkov@gmail.com>
2023-10-02 23:14:02 +0600

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const env = dotenv.config().parsed
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next])
+  return prev
+}, {})
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer:{
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new webpack.DefinePlugin(envKeys),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
